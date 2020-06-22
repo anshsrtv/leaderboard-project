6451ddf19e103b012ba454a455596bf7633b7d58
priyanshisharma <priyanshi417@gmail.com>
2020-06-22 20:10:49 +0530

    Modified Fields

--- profiles/models.py
+++ profiles/models.py
@@ -2,10 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Milestone(models.Model):
-    good_start = models.BooleanField(default=False)
-    terrific_start = models.BooleanField(default=False)
-    almost_there = models.BooleanField(default=False)
-    mission_accomplished = models.BooleanField(default=False)
+    good_first_issue = models.BooleanField(default=False) #Shall be true on completion of Good first issue
+    medium_question_1 = models.BooleanField(default=False) #Shall be true on completion of first medium issue
+    medium_question_2 = models.BooleanField(default=False) #Shall be true on completion of first medium issue
+    '''
+    We can identify labels for various situations as follows
+        1. good_start -> good_first_issue = True
+                        medium_question_1 = False
+                        medium_question_2 = False
+        2. terrific_start -> good_first_issue = False
+                        medium_question_1 = True
+                        medium_question_2 = False
+        3. almost_there -> good_first_issue = True
+                        medium_question_1 = True
+                        medium_question_2 = False
+        4. mission_accomplished -> good_first_issue = True
+                                    medium_question_1 = True
+                                    medium_question_2 = True
+    '''
 
 
 class Leaderboard(models.Model):
