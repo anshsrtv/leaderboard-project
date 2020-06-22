c50df6d7c825035aefd2a1da7715a1d3018eff3a
anshsrtv <ansh_srtv@outlook.com>
2020-06-22 23:30:46 +0530

    fix: Int fields

--- profiles/models.py
+++ profiles/models.py
@@ -5,10 +5,10 @@ class Leaderboard(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     points = models.IntegerField()
     good_first_issue = models.BooleanField(default=False) #Shall be true on completion of Good first issue
-    medium_issues_solved = models.IntegerField() # Count the number of medium issues solved.
-    hard_issues_solved = models.IntegerField() # Count the number of hard issues solved.
-    pr_opened = models.IntegerField() # Count the number of PRs opened.
-    pr_merged = models.IntegerField() # Count the number of PRs merged.
+    medium_issues_solved = models.IntegerField(default=0) # Count the number of medium issues solved.
+    hard_issues_solved = models.IntegerField(default=0) # Count the number of hard issues solved.
+    pr_opened = models.IntegerField(default=0) # Count the number of PRs opened.
+    pr_merged = models.IntegerField(default=0) # Count the number of PRs merged.
     '''
     We can identify labels for various situations as follows
         1. good_start -> good_first_issue = True
