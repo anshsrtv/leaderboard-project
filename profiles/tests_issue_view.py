e55511b90f3084b9ca8e80806ab983130401f2c6
Priyanshi Sharma <priyanshi417@gmail.com>
2020-06-24 18:04:54 +0530

    Update profiles/tests_issue_view.py
    Co-authored-by: anshsrtv <49693160+anshsrtv@users.noreply.github.com>

--- profiles/tests_issue_view.py
+++ profiles/tests_issue_view.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from profiles.models import Leaderboard
 
 
-class PullRequestTestCase(APITestCase):
+class IssueTestCase(APITestCase):
     '''
     This class is to test the issue view
     from profiles.views.py
