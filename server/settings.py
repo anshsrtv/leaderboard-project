0273d9bbcbb5ed3c6f1d714d29f0110c8310f4f0
Ansh Srivastava <ansh_srtv@outlook.com>
2021-02-01 16:32:46 +0530

    Update server/settings.py

--- server/settings.py
+++ server/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR + '/templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
